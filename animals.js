6a56c41c47a73835a2d7d9b068c8572fce0b681f
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-02-07 09:54:18 +0100

    randomizing result on search

--- animals.js
+++ animals.js
@@ -5,12 +5,9 @@ $(() => {
     const API_KEY = "8vIIuF5yMMxMuUSYsTIIrQ==KfWC81Zd84v21P0k";
     
 
-    const randomNumber = () => {
+    const getRandomNumber = () => {
        return Math.floor(Math.random() * 10); //replace 10 with amount of objects returned
     }
-
-    const getRandomNumber = randomNumber();
-    console.log("Number: " + getRandomNumber);
     
     
     $(".search-btn").on("click", () => {
@@ -25,23 +22,26 @@ $(() => {
             $(".note").empty().append(`<p>Don't be boring You're not getting a regular ${$("#user-search").val()}, whatever that is. Be a bit more specific next time.<br> This is what I got for you:</p>`);
         } else {$(".note").empty()};
     };
-
+    
     const getAnimalName = async (url, apiKey) => {
+        const randomNumber = getRandomNumber();
+        console.log("Number: " + randomNumber);
+        
         try {
             let response = await fetch(url, {headers: {'X-Api-Key': apiKey,}
-            });
-
-            if (!response.ok) {
-                throw new Error("Oh no! Something went wrong. Error code: " + response.status);
-            }
-
+        });
+        
+        if (!response.ok) {
+            throw new Error("Oh no! Something went wrong. Error code: " + response.status);
+        }
+        
             let data = await response.json();
-            let animal = data[0].name;
-            let length = data[0].characteristics.length || "Info missing, use your imagination"
-            let diet = data[0].characteristics.main_prey || "Unfortunately there's no info on this animals diet";
-            let location = data[0].locations || "You'll have to use your amazing geography skills for this one";
-            let fancyName = data[0].taxonomy.scientific_name || "Oh no! The smartfaces didn't come up with a fancy scientific name for this animal";
-            let slogan = data[0].characteristics.slogan || "Not popular enough to have a catchy slogan";
+            let animal = data[randomNumber].name;
+            let length = data[randomNumber].characteristics.length || "Info missing, use your imagination"
+            let diet = data[randomNumber].characteristics.main_prey || "Unfortunately there's no info on this animals diet";
+            let location = data[randomNumber].locations || "You'll have to use your amazing geography skills for this one";
+            let fancyName = data[randomNumber].taxonomy.scientific_name || "Oh no! The smartfaces didn't come up with a fancy scientific name for this animal";
+            let slogan = data[randomNumber].characteristics.slogan || "Not popular enough to have a catchy slogan";
 
             console.log(data);
             
